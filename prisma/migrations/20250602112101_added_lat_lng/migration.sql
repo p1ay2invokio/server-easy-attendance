6e732f4c1634b065e0d89bff63bd22940e59c441
p1ay2invokio <play21947@gmail.com>
2025-06-03 21:28:52 +0700

    version-0.1

---
+++ prisma/migrations/20250602112101_added_lat_lng/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Store" ADD COLUMN     "lat" DECIMAL(65,30),
+ADD COLUMN     "lng" DECIMAL(65,30);
--- routes/attendance.route.ts
+++ routes/attendance.route.ts
@@ -40,10 +40,12 @@ app.post("/attend", async (req: Request, res: Response) => {
     const today = dayjs().locale('th').format('DD-MM-YYYY')
 
     // Define TaveethongPlace latitude, longitude
-    const MaeKhanPlace = {
-        lat: 18.528631,
-        long: 98.8595009
-    }
+    // const MaeKhanPlace = {
+    //     lat: 18.528631,
+    //     long: 98.8595009
+    // }
+
+
 
 
     let userId = await prisma.employee.findFirst({
@@ -56,41 +58,53 @@ app.post("/attend", async (req: Request, res: Response) => {
     if (userId) {
 
         console.log(my_lat, my_long)
+
+        // Get Lat Long Place Working
+        let store_employee = await prisma.store.findFirst({
+            where: {
+                id: userId.store_id
+            }
+        })
+
+
         // Create object and pass the lat, long via class LatLon
-        const p1 = new LatLon(MaeKhanPlace.lat, MaeKhanPlace.long);
-        const p2 = new LatLon(18.528631, 98.8595009)
+        const p1 = new LatLon(Number(store_employee?.lat), Number(store_employee?.lng));
+        const p2 = new LatLon(my_lat, my_long)
+        // const p2 = new LatLon(18.528525, 98.859388)
 
         // Use Method for find meters of these points
         const distance = p1.distanceTo(p2)
 
-        if (distance <= 50) {
-            // Get today_attendance by date
-            const today_attendance = await prisma.attendance.findFirst({
-                where: {
-                    eid: Number(id),
-                    work_timestamp: today
-                }
-            })
-            if (today_attendance?.in_timestamp) {
-                res.status(400).send({ status: false, message: "เข้างานซ้ำไม่ได้!" })
-            } else {
-
-                await prisma.attendance.create({
-                    data: {
+        if (store_employee) {
+            if (distance <= store_employee?.radius) {
+                // Get today_attendance by date
+                const today_attendance = await prisma.attendance.findFirst({
+                    where: {
                         eid: Number(id),
-                        work_timestamp: today,
-                        in_timestamp: time_now,
-                        out_timestamp: 0
+                        work_timestamp: today
                     }
                 })
+                if (today_attendance?.in_timestamp) {
+                    res.status(400).send({ status: false, message: "เข้างานซ้ำไม่ได้!" })
+                } else {
 
-                res.status(200).send({ status: true, message: "เข้างานสำเร็จ!" })
+                    await prisma.attendance.create({
+                        data: {
+                            eid: Number(id),
+                            work_timestamp: today,
+                            in_timestamp: time_now,
+                            out_timestamp: 0
+                        }
+                    })
+
+                    res.status(200).send({ status: true, message: "เข้างานสำเร็จ!" })
+                }
+            } else {
+                res.status(400).send({ status: false, message: `คุณไม่ได้อยู่ในพื้นที่! ห่าง ${(distance / 1000).toFixed(2)} กิโลเมตร` })
             }
         } else {
-            res.status(400).send({ status: false, message: `คุณไม่ได้อยู่ในพื้นที่! ห่าง ${(distance/1000).toFixed(2)} กิโลเมตร` })
+            res.status(401).send({ status: false, message: "ไม่พบ id นี้ในระบบ" })
         }
-    } else {
-        res.status(401).send({ status: false, message: "ไม่พบ id นี้ในระบบ" })
     }
 
     // const hours = dayjs().hour()
@@ -104,7 +118,7 @@ app.post("/attend", async (req: Request, res: Response) => {
 
 app.post("/out", async (req: Request, res: Response) => {
 
-    const { id } = req.body
+    const { id, my_lat, my_long } = req.body
 
     if (!id) {
         res.status(400).send({ status: false, message: "id is missing!" })
@@ -131,25 +145,48 @@ app.post("/out", async (req: Request, res: Response) => {
 
     if (attendance) {
 
-        if (attendance && attendance.in_timestamp) {
 
-            if (attendance.out_timestamp) {
-                res.status(400).send({ status: false, message: "คุณออกงานไปแล้ว กรุณาอย่ากดซ้ำ!" })
-            } else {
-                const out_timestamp = dayjs().unix()
 
-                await prisma.attendance.update({
-                    data: {
-                        out_timestamp: out_timestamp
-                    }, where: {
-                        id: attendance.id
-                    }
-                })
-
-                res.status(200).send({ status: true, message: "ออกจากงานสำเร็จ!" })
+        // Get Lat Long Place Working
+        let store_employee = await prisma.store.findFirst({
+            where: {
+                id: employee?.store_id
             }
-        } else {
-            res.status(400).send({ status: false, message: "คุณยัังไม่ได้เข้างานไม่สามารถออกได้!" })
+        })
+
+
+        // Create object and pass the lat, long via class LatLon
+        const p1 = new LatLon(Number(store_employee?.lat), Number(store_employee?.lng));
+        const p2 = new LatLon(my_lat, my_long)
+        // const p2 = new LatLon(18.528525, 98.859388)
+
+        const distance = p1.distanceTo(p2)
+
+        if (store_employee) {
+            if (distance <= store_employee?.radius) {
+                if (attendance && attendance.in_timestamp) {
+
+                    if (attendance.out_timestamp) {
+                        res.status(400).send({ status: false, message: "คุณออกงานไปแล้ว กรุณาอย่ากดซ้ำ!" })
+                    } else {
+                        const out_timestamp = dayjs().unix()
+
+                        await prisma.attendance.update({
+                            data: {
+                                out_timestamp: out_timestamp
+                            }, where: {
+                                id: attendance.id
+                            }
+                        })
+
+                        res.status(200).send({ status: true, message: "ออกจากงานสำเร็จ!" })
+                    }
+                } else {
+                    res.status(400).send({ status: false, message: "คุณยัังไม่ได้เข้างานไม่สามารถออกได้!" })
+                }
+            } else {
+                res.status(400).send({ status: false, message: `ไม่สามารถออกงานได้ คุณไม่ได้อยู่ในพื้นที่! ห่าง ${(distance / 1000).toFixed(2)} กิโลเมตร` })
+            }
         }
 
     } else {
@@ -157,23 +194,25 @@ app.post("/out", async (req: Request, res: Response) => {
     }
 })
 
-app.get("/history/:id", async(req: Request, res: Response)=>{
-    const {id} = req.params
+app.get("/history/:id", async (req: Request, res: Response) => {
+    const { id } = req.params
 
-    if(!id){
-        res.status(401).send({status: false, message: "id is missing"})
+    if (!id) {
+        res.status(401).send({ status: false, message: "id is missing" })
     }
 
-    let history_user = await prisma.attendance.findMany({where:{
-        eid: Number(id),
-        out_timestamp: {
-            not: 0
+    let history_user = await prisma.attendance.findMany({
+        where: {
+            eid: Number(id),
+            out_timestamp: {
+                not: 0
+            }
+        }, orderBy: {
+            id: "desc"
         }
-    },orderBy:{
-        id: "desc"
-    }})
+    })
 
-    res.status(200).send({status: true, data: history_user})
+    res.status(200).send({ status: true, data: history_user })
 })
 
 
